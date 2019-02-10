76f142240160658e79e9a1bd4d6dc918df959896
Ronan Doherty <donanroherty@gmail.com>
2019-02-10 23:09:08 +0000

    Created dropdown menu

---
+++ src/data/Types.ts
@@ -0,0 +1,20 @@
+export enum Key {
+  C = 'C',
+  CSharp = 'C#',
+  D = 'D',
+  DSharp = 'D#',
+  E = 'E',
+  F = 'F',
+  FSharp = 'F#',
+  G = 'G',
+  GSharp = 'G#',
+  A = 'A',
+  ASharp = 'A#',
+  B = 'B'
+}
+
+// export const keyAsString = (key: Key) => {
+//   return Object.values(KeyString)[key]
+// }
+
+// export const keyStringArray = () => Object.keys(KeyString)
---
+++ src/ui/Dropdown.tsx
@@ -0,0 +1,150 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import styled, { withTheme } from 'styled-components'
+import Icon from './Icon'
+import { ThemeInterface } from '../theme/theme'
+
+interface DropdownProps {
+  width?: string
+  dropdownHeight?: string
+  options: Array<string>
+  selected: number
+  handleOptionSelection?: (idx: number) => void
+  borderRadius?: string
+  theme?: ThemeInterface
+}
+
+interface DropdownState {
+  isOpen: boolean
+}
+
+class Dropdown extends React.Component<DropdownProps, DropdownState> {
+  constructor(props: DropdownProps) {
+    super(props)
+
+    this.state = {
+      isOpen: false
+    }
+  }
+  static defaultProps: DropdownProps = {
+    width: '200px',
+    dropdownHeight: '100px',
+    options: ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+    selected: 0,
+    borderRadius: '10px'
+  }
+
+  componentWillMount = () => document.addEventListener('mousedown', this.handleClick, false)
+  componentWillUnmount = () => document.removeEventListener('mousedown', this.handleClick, false)
+  // Close menu on outside click
+  handleClick = (e: any) => {
+    const domNode = ReactDOM.findDOMNode(this)
+    if (!domNode || !domNode.contains(e.target)) {
+      this.closeMenu()
+    }
+  }
+
+  toggleMenuOpen = (e: any) => this.setState({ isOpen: !this.state.isOpen })
+  closeMenu = () => this.setState({ isOpen: false })
+
+  handleOptionClick = (idx: number) => {
+    this.props.handleOptionSelection!(idx)
+    this.closeMenu()
+  }
+
+  render() {
+    const optionComps = this.props.options!.map((val, i) => {
+      return (
+        <Option {...this.props} onClick={() => this.handleOptionClick(i)} key={val + i}>
+          {val}
+        </Option>
+      )
+    })
+
+    return (
+      <Wrapper {...this.props}>
+        <TopSection {...this.props} isOpen={this.state.isOpen} onClick={this.toggleMenuOpen}>
+          <Selection>
+            <div>{this.props.options![this.props.selected]}</div>
+          </Selection>
+          <ChevronWrapper>
+            <Icon icon="chevron" fillColor={this.props.theme!.primary} size={6} />
+          </ChevronWrapper>
+        </TopSection>
+        <OptionPanelWrapper isOpen={this.state.isOpen}>
+          <OptionPanel {...this.props}>{optionComps}</OptionPanel>
+        </OptionPanelWrapper>
+      </Wrapper>
+    )
+  }
+}
+
+const Wrapper = styled.div<DropdownProps>`
+  width: ${props => props.width};
+  background-color: white;
+  font-size: 14px;
+  font-weight: bold;
+  user-select: none;
+  border: 1px solid ${props => props.theme.primaryLight};
+  border-radius: ${props => props.borderRadius};
+  overflow: hidden;
+`
+const ChevronWrapper = styled.div`
+  width: 38px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  filter: drop-shadow(${props => props.theme.dropShadow});
+`
+const TopSection = styled.div<any>`
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+
+  border-radius: ${props =>
+    `${props.borderRadius} ${props.borderRadius} ${props.isOpen ? '0' : props.borderRadius} ${
+      props.isOpen ? '0' : props.borderRadius
+    }`};
+
+  &:hover {
+    background-color: ${props => props.theme.primaryVeryLight};
+  }
+  &:hover ${ChevronWrapper} {
+    filter: brightness(${props => props.theme.hoverBrightness})
+      drop-shadow(${props => props.theme.hoverDropShadow});
+  }
+  &:active ${ChevronWrapper} {
+    filter: brightness(${props => 1 - (props.theme.hoverBrightness - 1)});
+  }
+`
+const Selection = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 14px;
+`
+
+const OptionPanelWrapper = styled.div<any>`
+  display: ${props => (props.isOpen ? 'inherit' : 'none')};
+`
+const OptionPanel = styled.div<DropdownProps>`
+  height: ${props => props.dropdownHeight};
+  overflow-y: auto;
+  border-top: 1px solid ${props => props.theme.primaryLight};
+`
+const Option = styled.div`
+  height: 35px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 14px;
+  &:hover {
+    background-color: ${props => props.theme.primaryVeryLight};
+  }
+`
+
+export default withTheme(Dropdown)
