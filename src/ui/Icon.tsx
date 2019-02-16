ba1dd2044604deb39ec01413b4c8951bd7068f2b
Ronan Doherty <donanroherty@gmail.com>
2019-02-16 15:20:44 +0000

    Changes to allow multiple colors for icons

--- src/ui/Icon.tsx
+++ src/ui/Icon.tsx
@@ -7,15 +7,20 @@
 import React from 'react'
 import styled from '../theme/themed-styled-components'
 import iconDefinitions, { getIconDimensions } from './iconDefinitions'
+import shortId from 'shortid'
 
 export interface IconProps {
   fillColor?: string
+  fillColor2?: string
+  fillColor3?: string
   size?: number
   icon?: keyof typeof iconDefinitions
 }
 
 const defaultProps: IconProps = {
-  fillColor: 'black',
+  fillColor: 'pink',
+  fillColor2: 'lightgreen',
+  fillColor3: 'lightblue',
   size: 50,
   icon: 'pause'
 }
@@ -23,11 +28,31 @@ const defaultProps: IconProps = {
 const Icon = (props: IconProps) => {
   // Get viewbox dimensions from icon definition
   const iconDef = iconDefinitions[props.icon!]
-  const viewBox = iconDef.props.viewBox.split(' ')
-  const vbWidth = viewBox[2]
-  const vbHeight = viewBox[3]
+  const viewBox = iconDef.viewBox
+  const vbWidth = viewBox.x2 - viewBox.x1
+  const vbHeight = viewBox.y2 - viewBox.y1
 
   const dimensions = getIconDimensions(props.icon!, props.size!)
+
+  const paths = iconDef.paths.map((val, i) => {
+    console.log(val)
+
+    const color = val.props.className
+      ? val.props.className === 'color1'
+        ? props.fillColor
+        : val.props.className === 'color2'
+          ? props.fillColor2
+          : val.props.className === 'color3'
+            ? props.fillColor3
+            : 'red'
+      : props.fillColor
+
+    return (
+      <SVGInner key={props.icon! + ' icon ' + shortId.generate()} fillColor={color}>
+        {val}
+      </SVGInner>
+    )
+  })
 
   return (
     <SVGWrapper
@@ -36,12 +61,15 @@ const Icon = (props: IconProps) => {
       height={dimensions.height}
       width={dimensions.width}
     >
-      {iconDef}
+      {paths}
     </SVGWrapper>
   )
 }
 
-const SVGWrapper = styled('svg')<IconProps>`
+const SVGWrapper = styled.svg<IconProps>`
+  fill: ${props => props.fillColor};
+`
+const SVGInner = styled.svg<any>`
   fill: ${props => props.fillColor};
 `
 
