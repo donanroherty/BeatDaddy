0f386b34c74e82e72a03887013ad70050670e1d1
Ronan Doherty <donanroherty@gmail.com>
2019-03-06 14:25:02 +0000

    Improved drone volume control

--- src/audio-engines/Drone.tsx
+++ src/audio-engines/Drone.tsx
@@ -16,15 +16,12 @@ interface DroneProps {
 interface DroneState {}
 
 class Drone extends React.Component<DroneProps, DroneState> {
-  private keys: string[] = []
   private synth = new Tone.PolySynth(4, Tone.Synth).toMaster()
-  private volume = new Tone.Volume(0)
   private chordTones: string[] = new Array()
 
   constructor(props: DroneProps) {
     super(props)
 
-    this.synth.chain(this.volume, Tone.Master)
     this.synth.volume.rampTo(this.getSafeVolume(), 0)
 
     this.handleChordChanged()
@@ -43,13 +40,17 @@ class Drone extends React.Component<DroneProps, DroneState> {
     }
 
     if (prevProps.volume !== this.props.volume) {
-      this.synth.volume.rampTo(this.getSafeVolume(), 0)
+      if (this.props.volume === 0) {
+        this.synth.volume.rampTo(-100, 0)
+      } else {
+        this.synth.volume.rampTo(this.getSafeVolume(), 0)
+      }
     }
   }
 
   // Returns volume prop mapped within a usable range for the drone
   getSafeVolume = () => {
-    const min = -20
+    const min = -40
     const max = 0
     return mapToRange(this.props.volume, 0, 100, min, max)
   }
