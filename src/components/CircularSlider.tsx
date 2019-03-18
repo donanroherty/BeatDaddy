64fbef4934e40472e8d6f3e3d0aa6d555d7c3867
Ronan Doherty <donanroherty@gmail.com>
2019-03-18 22:37:27 +0000

    Hooked up tempo slider to state

--- src/components/CircularSlider.tsx
+++ src/components/CircularSlider.tsx
@@ -1,6 +1,6 @@
 import React, { MouseEvent } from 'react'
 import styled, { withTheme } from 'styled-components'
-import { mapToRange, clamp } from '../utils/utils'
+import { mapToRange, clamp, degToRad, polarToCartesian, makeArc } from '../utils/utils'
 import { ThemeInterface } from '../theme/theme'
 import Icon from '../ui/Icon'
 import { getIconDimensions } from '../ui/iconDefinitions'
@@ -19,7 +19,7 @@ interface CircularSliderProps {
 }
 
 interface CircularSliderState {
-  draggingThumbMarker: boolean
+  isDragging: boolean
   initialized: boolean
 }
 
@@ -37,52 +37,87 @@ const defaultProps: CircularSliderProps = {
 class CircularSlider extends React.Component<CircularSliderProps, CircularSliderState> {
   private elementRef: React.RefObject<HTMLDivElement> = React.createRef<HTMLDivElement>()
   private pathRef: React.RefObject<SVGPathElement> = React.createRef<SVGPathElement>()
-  private thumbMarkerRef: React.RefObject<SVGSVGElement> = React.createRef<SVGSVGElement>()
+  private sliderHandleRef: React.RefObject<SVGSVGElement> = React.createRef<SVGSVGElement>()
 
   constructor(props: CircularSliderProps) {
     super(props)
 
     this.state = {
-      draggingThumbMarker: false,
+      isDragging: false,
       initialized: false
     }
   }
 
-  componentDidUpdate(prevProps: CircularSliderProps, prevState: CircularSliderState) {
-    if (prevProps.value !== this.props.value) {
-      // this.updateThumbMarkerPosition()
-    }
-  }
+  static defaultProps = defaultProps
 
   componentWillMount() {
-    document.addEventListener('mouseup', this.handleClick, false)
+    document.addEventListener('mouseup', this.onMouseUp, false)
+    document.addEventListener('touchend', this.onMouseUp, false)
+    document.addEventListener('mousemove', this.onMouseMove, false)
+    document.addEventListener('touchmove', this.onTouchMove, false)
   }
   componentWillUnmount() {
-    document.removeEventListener('mouseup', this.handleClick, false)
+    document.removeEventListener('mouseup', this.onMouseUp, false)
+    document.removeEventListener('touchend', this.onMouseUp, false)
+    document.removeEventListener('mousemove', this.onMouseMove, false)
+    document.removeEventListener('touchmove', this.onTouchMove, false)
   }
 
   componentDidMount() {
-    // this.updateThumbMarkerPosition()
+    // Render again after first render when refs are set
     this.setState({ initialized: true })
   }
 
-  handleClick = (e: any) => {
-    this.setState({ draggingThumbMarker: false })
+  onMouseUp = (e: any) => {
+    if (!this.state.isDragging) return
+    e.stopPropagation()
+    e.preventDefault()
+    this.setState({ isDragging: false })
+  }
+  onMouseDown = (e: any) => {
+    e.stopPropagation()
+    e.preventDefault()
+    this.setState({ isDragging: true })
+  }
+  onMouseMove = (e: any) => {
+    if (!this.state.isDragging) return
+    e.stopPropagation()
+    e.preventDefault()
+    if (!this.state.initialized) return
+    const pos: Vector2D = { x: e.pageX, y: e.pageY }
+    this.handleValueUpdate(pos)
+  }
+  onPathMouseDown = (e: MouseEvent) => {
+    if (this.state.isDragging) return
+    e.stopPropagation()
+    e.preventDefault()
+    const pos: Vector2D = { x: e.pageX, y: e.pageY }
+    this.handleValueUpdate(pos)
+  }
+  onPathTouchStart = (e: any) => {
+    if (this.state.isDragging) return
+    e.stopPropagation()
+    e.preventDefault()
+    const pos: Vector2D = { x: e.touches[0].clientX, y: e.touches[0].clientY }
+    this.handleValueUpdate(pos)
+  }
+  onTouchMove = (e: TouchEvent) => {
+    if (!this.state.isDragging) return
+    e.stopPropagation()
+    e.preventDefault()
+    const pos: Vector2D = { x: e.touches[0].clientX, y: e.touches[0].clientY }
+    this.handleValueUpdate(pos)
   }
 
-  static defaultProps = defaultProps
+  handleValueUpdate = (pos: Vector2D) => {
+    if (!this.state.initialized) return
 
-  handlePointerMove = (e: MouseEvent) => {
-    if (!this.state.draggingThumbMarker) return
-    if (!this.elementRef.current) return
-
-    const rect = this.elementRef.current.getBoundingClientRect()
-
+    const rect = this.elementRef.current!.getBoundingClientRect()
     const center: Vector2D = {
       x: rect.left + window.scrollX + rect.width * 0.5,
       y: rect.top + window.scrollY + rect.height * 0.5
     }
-    const mousePos: Vector2D = { x: e.pageX, y: e.pageY }
+    const mousePos: Vector2D = { x: pos.x, y: pos.y }
     const rel: Vector2D = { x: mousePos.x - center.x, y: mousePos.y - center.y }
 
     const atan2 = Math.atan2(rel.y, rel.x)
@@ -90,52 +125,31 @@ class CircularSlider extends React.Component<CircularSliderProps, CircularSlider
     const rotated = degrees - 270 < 0 ? degrees - 270 + 360 : degrees - 270
     const clamped = clamp(rotated, this.props.sliceAngle!, 360 - this.props.sliceAngle!)
     const mapped = mapToRange(clamped, this.props.sliceAngle!, 360 - this.props.sliceAngle!, 0, 100)
-
     this.props.setValue!(mapped)
   }
 
-  degToRad = (angleInDegrees: number) => (angleInDegrees - 90) * (Math.PI / 180.0)
+  getSliderHandlePosition = () => {
+    if (!this.pathRef.current || !this.sliderHandleRef.current) return
+    const arcLength = this.pathRef.current.getTotalLength()
+    const mappedAngle = arcLength - mapToRange(this.props.value!, 0, 100, 0, arcLength)
 
-  polarToCartesian = (centerX: number, centerY: number, radius: number, angleInDegrees: number) => {
-    const a = this.degToRad(angleInDegrees)
-    return {
-      x: centerX + radius * Math.cos(a),
-      y: centerY + radius * Math.sin(a)
-    }
-  }
+    const pos = this.pathRef.current.getPointAtLength(mappedAngle)
 
-  makeArc = (x: number, y: number, radius: number, startAngle: number, endAngle: number) => {
-    const fullCircle = endAngle - startAngle === 360
-    const start = this.polarToCartesian(x, y, radius, endAngle - 0.01)
-    const end = this.polarToCartesian(x, y, radius, startAngle)
-    const arcSweep = endAngle - startAngle <= 180 ? '0' : '1'
-
-    const d = `M ${start.x} ${start.y} A ${radius} ${radius} 0 ${arcSweep} 0 ${end.x} ${end.y}${
-      fullCircle ? 'z' : ''
-    }`
-
-    return d
-  }
-
-  handleThumbMarkerClick = (e: any) => {
-    console.log('click')
-    this.setState({ draggingThumbMarker: true })
+    return { x: pos.x - this.props.thumbRadius!, y: pos.y - this.props.thumbRadius! }
   }
 
   render() {
     const { width, height, sliceAngle, radius, strokeWidth, thumbRadius, theme, value } = this.props
 
-    const arcCenter: Vector2D = {
-      x: width! * 0.5,
-      y: height! * 0.5
-    }
+    const arcCenter: Vector2D = { x: width! * 0.5, y: height! * 0.5 }
     const arcRad = radius! - strokeWidth! / 2
     const arcStartAng = 180 + sliceAngle!
     const arcEndAng = 360 + 180 - sliceAngle!
-    const arcStart = this.polarToCartesian(arcCenter.x, arcCenter.y, arcRad, arcEndAng - 0.01)
-    const arcEnd = this.polarToCartesian(arcCenter.x, arcCenter.y, arcRad, arcStartAng)
+    const arcStart = polarToCartesian(arcCenter.x, arcCenter.y, arcRad, arcEndAng - 0.01)
+    const arcEnd = polarToCartesian(arcCenter.x, arcCenter.y, arcRad, arcStartAng)
 
-    const arc = this.makeArc(
+    // The primary circular path everything is built on
+    const arc = makeArc(
       width! * 0.5,
       height! * 0.5,
       radius! - strokeWidth! / 2,
@@ -144,33 +158,10 @@ class CircularSlider extends React.Component<CircularSliderProps, CircularSlider
     )
 
     const arcLength = this.pathRef.current ? this.pathRef.current.getTotalLength() : 0
-
     const grabIconDims = getIconDimensions('grabDots', 15)
 
-    const getThumbMarkerPosition = () => {
-      if (!this.pathRef.current || !this.thumbMarkerRef.current) return
-
-      const arcLength = this.pathRef.current.getTotalLength()
-      const mappedAngle = arcLength - mapToRange(value!, 0, 100, 0, arcLength)
-      const pos = this.pathRef.current.getPointAtLength(mappedAngle)
-
-      const out: Vector2D = {
-        x: pos.x - thumbRadius!,
-        y: pos.y - thumbRadius!
-      }
-
-      return out
-    }
-
     return (
-      <Wrapper
-        {...this.props}
-        onMouseMove={this.handlePointerMove.bind(this)}
-        id="circular-slider"
-        ref={this.elementRef}
-      >
-        {/* Extends bounds of component to catch mouse events */}
-        {/* <MouseCatcher /> */}
+      <Wrapper {...this.props} ref={this.elementRef}>
         <SVGContainer viewBox={`0 0 ${width} ${height}`}>
           {/* Background fill */}
           <circle
@@ -178,7 +169,6 @@ class CircularSlider extends React.Component<CircularSliderProps, CircularSlider
             cx={this.props.radius! + 0.25 + this.props.thumbRadius! / 2}
             cy={this.props.radius! + 0.25 + this.props.thumbRadius! / 2}
             fill={'white'}
-            onClick={e => console.log('click')}
             style={{
               userSelect: 'none'
             }}
@@ -194,6 +184,8 @@ class CircularSlider extends React.Component<CircularSliderProps, CircularSlider
             style={{
               userSelect: 'none'
             }}
+            onClick={this.onPathMouseDown}
+            onTouchStart={this.onPathTouchStart}
           />
 
           {/* Fill path */}
@@ -207,6 +199,8 @@ class CircularSlider extends React.Component<CircularSliderProps, CircularSlider
               strokeDashoffset: (arcLength / 100) * this.props.value!,
               userSelect: 'none'
             }}
+            onClick={this.onPathMouseDown}
+            onTouchStart={this.onPathTouchStart}
             d={arc}
           />
 
@@ -220,6 +214,7 @@ class CircularSlider extends React.Component<CircularSliderProps, CircularSlider
               userSelect: 'none'
             }}
           />
+
           {/* Start cap */}
           <circle
             r={strokeWidth! / 2}
@@ -233,19 +228,18 @@ class CircularSlider extends React.Component<CircularSliderProps, CircularSlider
 
           {/* Thumb marker */}
           <svg
-            ref={this.thumbMarkerRef}
-            onMouseDown={this.handleThumbMarkerClick}
-            x={this.state.initialized ? getThumbMarkerPosition()!.x : 0}
-            y={this.state.initialized ? getThumbMarkerPosition()!.y : 0}
+            ref={this.sliderHandleRef}
+            onMouseDown={this.onMouseDown}
+            onTouchStart={this.onMouseDown}
+            x={this.state.initialized ? this.getSliderHandlePosition()!.x : 0}
+            y={this.state.initialized ? this.getSliderHandlePosition()!.y : 0}
           >
             <circle r={thumbRadius} cx={thumbRadius} cy={thumbRadius} fill={theme!.primaryLight} />
-
             <svg x={thumbRadius! - grabIconDims.width / 2} y={thumbRadius! / 2}>
               <Icon icon="grabDots" size={15} fillColor={theme!.primary} />
             </svg>
           </svg>
         </SVGContainer>
-        )
       </Wrapper>
     )
   }
@@ -256,15 +250,9 @@ const Wrapper = styled.div<CircularSliderProps>`
   z-index: 10;
   width: ${props => props.width}px;
   height: ${props => props.height}px;
+  touch-action: none;
 `
-const MouseCatcher = styled.div`
-  position: fixed;
-  z-index: 0;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-`
+
 const SVGContainer = styled.svg`
   position: relative;
 `
